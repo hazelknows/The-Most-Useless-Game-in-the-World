bf8827802df6471b7f2dca2410042980700e65fe
Lisa <ryiahtyisha777@gmail.com>
2025-03-25 18:54:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Load the click sound
+    const clickSound = new Audio("click.mp3"); 
+
+    // Select all buttons
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            clickSound.currentTime = 0; // Reset sound in case of rapid clicks
+            clickSound.play();
+        });
+    });
+});
